15f0d2f3a4a59b2ba9891acfb9337fead455f6ef
Alaa Mousa <alaa.mousa025@gmail.com>
2026-09-08 19:00:30 +0300

    Time: 2 ms (32.48%) | Memory: 55.1 MB (26.87%) - LeetSync

---
+++ 682-baseball-game/baseball-game.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string[]} operations
+ * @return {number}
+ */
+var calPoints = function(operations) {
+    const stack = [];
+
+    for (const op of operations) {
+        if (op === "C") {
+            stack.pop();
+        } 
+        else if (op === "D") {
+            stack.push(stack[stack.length - 1] * 2);
+        } 
+        else if (op === "+") {
+            const last = stack[stack.length - 1];
+            const secondLast = stack[stack.length - 2];
+
+            stack.push(last + secondLast);
+        } 
+        else {
+            stack.push(Number(op));
+        }
+    }
+
+    return stack.reduce((sum, score) => sum + score, 0);
+};
